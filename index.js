1dcad8f4ea81a11d7e1c151db95bcf2dfa11dbf6
Valjean Clark <valjeanbud@gmail.com>
2015-03-11 19:34:58 -0700

    whiteboard working again

--- index.js
+++ index.js
@@ -20,13 +20,32 @@ if( process.env.NODE_ENV === 'production') {
 
 
 
+var dbconn, table;
 
-// Whiteboards (unique ID mapped to an object containing whiteboard metadata, i.e. name, and an array of whiteboard data)
-var whiteboards = {},
-		rooms = {};
+// get the whiteboards from the db
+var getWhiteboards = function(callback) {
+	table
+		.pluck('id', 'name')
+		.run(dbconn, function(err, cursor) {
+			cursor.toArray(function(err, whiteboards) {
+				callback(whiteboards);
+			});
+		});
+}
+
+var sendWhiteboards = function(socket, type, callback) {
+	getWhiteboards( function(whiteboards) {
+		console.log('Sending ' + whiteboards.length + ' whiteboards.');
+		// send the whiteboard list to all connected clients
+		if(type === 'reply')
+			socket.emit('updateWhiteboardList', whiteboards);
+		else if(type === 'broadcast')
+			io.emit('updateWhiteboardList', whiteboards);
+		if(typeof callback === 'function') callback();
+	});
+}
 
 // open DB connection
-var dbconn, table;
 r.connect({host: '127.0.0.1', port: 28015}, function(err, conn) {
 	if (err) throw err;
 	dbconn = conn;
@@ -34,143 +53,109 @@ r.connect({host: '127.0.0.1', port: 28015}, function(err, conn) {
 	console.log('Connected to RethinkDB.');
 
 
+
+	// setup listeners for when a client connects
 	io.on('connection', function(socket) {
 
-		// send a list of whiteboards to the client
-		console.log('New client connected, sending available whiteboards.');
-		table.pluck('id', 'name').run(dbconn, function(err, cursor) {
-			cursor.toArray(function(err, whiteboards) {
-				debugger;
-				console.log('Sent whiteboards: ', whiteboards.length);
-				socket.emit('hello', whiteboards);
-			});
-		});
+		// say hello and send the whiteboards to the client
+		socket.emit('hello');
+		console.log('New client connected, saying hello.');
+		sendWhiteboards(socket, 'reply');
 
 		// Client is asking to join a particular whiteboard
 		socket.on('joinWhiteboard', function(id) {
-			table.get(id).run(dbconn, function(err, result){
-				if(err) throw err;
-				console.log('Client wants to join whiteboard ' + id + '.');
-				socket.join(id); // join the client to the room for the specified whiteboard
-				socket.emit('updateWhiteboard', result); // sends the whiteboard object
-			});
+			table
+				.get(id)
+				.run(dbconn, function(err, result){
+					if(err) throw err;
+					console.log('Client wants to join whiteboard ' + id + '.');
+					socket.join(id); // join the client to the room for the specified whiteboard
+					console.log('Replying with whiteboard data.');
+					socket.emit('updateWhiteboard', result);
+				});
 		});
 
 		// Client wants to create a new whiteboard
 		socket.on('createWhiteboard', function(name) {
-			table.insert({id: uuid.v4(), name: name}).run(dbconn, function(err, result) {
-				if(err) throw err;
-				socket.emit('whiteboardCreated', result);
-			});
+			table
+				.insert({
+					id: uuid.v4(),
+					name: name,
+					data: []
+				}, { returnChanges: true })
+				.run(dbconn, function(err, result) {
+					if(err) throw err;
+					sendWhiteboards(socket, 'broadcast');
+					console.log('Replying with whiteboard data.');
+					socket.emit('updateWhiteboard', result.changes[0].new_val); // reply with the whiteboard object
+				});
 		});
 
-		// Client is sending new name for a whiteboard
-		socket.on('changeWhiteboardName', function(d) { // 'd' holds two properties, 'id' and 'name'
-			table.get(d.id).update({name: d.name}, {return_vals: true}).run(dbconn, function(err, result){
-				if(err) throw err;
-				console.log('Whiteboard (id: ' + d.id + ') data updated.');
-				socket.to(d.id).broadcast.emit('whiteboardNameChanged', result.new_val);
-			});
+		// Client wants to delete a whiteboard
+		socket.on('deleteWhiteboard', function(id) {
+			table
+				.get(id)
+				.delete()
+				.run(dbconn, function(err, result){
+					if(err) throw err;
+					console.log('Whiteboard ' + id + ' deleted.');
+					sendWhiteboards(socket, 'broadcast');
+				});
+		});		
+
+		// Client is sending a new name (d.name) and/or new data (d.data) for a whiteboard (specified by d.id)
+		socket.on('updateWhiteboard', function(d) {
+			table
+				.get(d.id)
+				.update(_.pick(d, ['name', 'data']), {returnChanges: true})
+				.run(dbconn, function(err, result){
+					if(err) throw err;
+					console.log('Whiteboard ' + d.id + ' ' + Object.keys(d).join(' and ') + ' properties updated.');
+
+					io.to(d.id).emit('updateWhiteboard', result.changes[0].new_val);
+				});
 		});
 
-		socket.on('addElements', function(d) { // 'd' holds two properties: 'id' and 'elements'
+		// More granular whiteboard data addition (instead of overwriting all the whiteboard data every time)
+		// Client is adding new elements (d.elements) to a whiteboard (specified by d.id)
+		socket.on('addElements', function(d) {
 			if(!d.id || !d.elements) return;
-			// emit to clients...
-			socket.to(d.id).broadcast.emit('elementsAdded', d);
+			// emit to all clients joined to this whiteboard...
+			io.to(d.id).emit('elementsAdded', d);
 			// ... then save to DB
 			table
 				.get(d.id)
 				.update({data: r.row('data')
-				.default([])
-				.setUnion(d.elements)})
+						.default([])
+						.setUnion(d.elements)
+				})
 				.run(dbconn, function(err, result){
 					if(err) throw err;
-					console.log('Element added: ', d.elements, result);
+					console.log(d.elements.length + ' elements added to whiteboard '  + d.id);
 				});
 		});
 
-		socket.on('removeElements', function(d) { // 'd' holds two properties: 'id' and 'elements'
+		// More granular whiteboard data removal (instead of overwriting all the whiteboard data every time)
+		// Client is removing elements (d.elements) from a whiteboard (specified by d.id)
+		socket.on('removeElements', function(d) {
 			if(!d.id || !d.elements) return;
-			// emit to clients...
-			socket.to(d.id).broadcast.emit('elementsRemoved', d);
+			// emit to all clients joined to this whiteboard...
+			io.to(d.id).emit('elementsRemoved', d);
 			// ... then save to DB
-			table.get(d.id).update({data: r.row('data')
-						.default([]).difference(d.elements)}).run(dbconn, function(err, result){
-				console.log('Element removed: ', d.element, result);
-			});
+			table
+				.get(d.id)
+				.update({
+					data: r.row('data')
+						.default([])
+						.difference(d.elements)
+				})
+				.run(dbconn, function(err, result){
+					console.log(d.elements.length + ' elements removed from whiteboard '  + d.id);
+				});
 		});
 	});
+	
 });
-
-/*
- // Whiteboards (unique ID mapped to an object containing whiteboard metadata, i.e. name, and an array of whiteboard data)
- var whiteboards = {},
- rooms = {};
-
- //TODO: get whiteboards from a database
- // Setup some whiteboards
- _.times(5, function(n) {
- createWhiteboard('wb' + (n + 1));
- });
-
- // Creates new whiteboard
- function createWhiteboard(name) {
- var newId = uuid.v4();
- whiteboards[ newId ] = {
- id: newId,
- name: name,
- data: []
- };
- }
- */
-
-//TODO: I'm currently just sending the whole whiteboard object over and over again. There are several things to improve.
-// 1: just send diffs to the right rooms. I'll need some logic to handle ordering if I do this
-// 2: Update Whiteboard Name event for clients as well?
-//io.on('connection', function(socket) {
-//	console.log('New client connected, sending available whiteboards.');
-//
-//	r.table('whiteboard').pluck('id', 'name').run(dbconn, function(err, cursor) {
-//		cursor.toArray(function(err, whiteboards) {
-//			console.log('Sent: ', whiteboards.length);
-//			socket.emit('hello', whiteboards);
-//		}); //sends an array of objects defining 'id' and 'name' of available whiteboards (don't send the data right now)
-////		cursor.each(function(err, row){
-////			if(err) throw err;
-////			socket.emit('hello')
-////		});
-////		socket.emit('hello', _.map(whiteboards, function(obj) {
-////		return _.pick(obj, 'id', 'name');
-////	}));
-//	});
-//
-//
-//	// Client is asking to join a particular whiteboard
-//	socket.on('joinWhiteboard', function(id) {
-//		console.log('Client wants to join whiteboard ' + id + '.')
-//		socket.join(id); //join the client to the room for the specified whiteboard
-//		socket.emit('updateWhiteboard', whiteboards[id]); //sends the whiteboard object
-//	});
-//
-//	// Client is sending new data for a whiteboard
-//	socket.on('updateWhiteboardData', function(d) { // 'd' holds two properties: 'id' and 'data'
-//		// Update the whiteboard specified by 'id'
-//		whiteboards[d.id].data = d.data;
-//		console.log('Whiteboard (id: ' + d.id + ') data updated.');
-//		// Send the updated whiteboard to all other clients using the same whiteboard
-//		socket.to(d.id).broadcast.emit('updateWhiteboard', whiteboards[d.id]);
-//	});
-//
-//	// Client is sending new name for a whiteboard
-//	socket.on('updateWhiteboardName', function(d) { // 'd' holds two properties, 'id' and 'name'
-//		// Update the whiteboard specified by 'id'
-//		whiteboards[d.id].name = d.name;
-//		console.log('Whiteboard (id: ' + d.id + ') name updated.');
-//		// Send the updated whiteboard to all other clients using the same whiteboard
-//		socket.to(id).broadcast.emit('updateWhiteboard', whiteboards[d.id]);
-//	})
-//
-//});
 
 http.listen(port, function(){
   console.log('Listening on port ' + port);
